eac566ea10888df06865533cb60d19f31b0e92ae
Dominik <dmkk1@outlook.com>
2022-03-13 15:35:09 +0100

    Add 2d primitive references

--- src/references.ts
+++ src/references.ts
@@ -257,4 +257,323 @@ export const references = [
             description: "URL params"
         }
     },
+    {
+        element: "arc",
+        description: "Draw an arc to the screen. If called with only x, y, w, h, start and stop, the arc will be drawn and filled as an open pie segment. If a mode parameter is provided, the arc will be filled like an open semi-circle (OPEN), a closed semi-circle (CHORD), or as a closed pie segment (PIE).",
+        code: "arc(x, y, w, h, start, stop, [mode], [detail])",
+        insert: "arc(${1:x}, ${2:y}, ${3:w}, ${4:h}, ${5:start}, ${6:stop})",
+        syntax: [
+            {
+                parameter: "x: Number",
+                description: "x-coordinate of the arc's ellipse"
+            },
+            {
+                parameter: "y: Number",
+                description: "y-coordinate of the arc's ellipse"
+            }, 
+            {
+                parameter: "w: Number",
+                description: "width of the arc's ellipse by default"
+            },
+            {
+                parameter: "h: Number",
+                description: "height of the arc's ellipse by default"
+            },   
+            {
+                parameter: "start: Number",
+                description: "angle to start the arc, specified in radians"
+            },
+            {
+                parameter: "stop: Number",
+                description: "angle to stop the arc, specified in radians"
+            },   
+            {
+                parameter: "mode: Constant",
+                description: "optional parameter to determine the way of drawing the arc. either CHORD, PIE or OPEN (Optional)"
+            },
+            {
+                parameter: "detail: Integer",
+                description: "optional parameter for WebGL mode only. This is to specify the number of vertices that makes up the perimeter of the arc. Default value is 25. Won't draw a stroke for a detail of more than 50. (Optional)"
+            },            
+        ]
+    },
+    {
+        element: "ellipse",
+        description: "Draws an ellipse (oval) to the screen. By default, the first two parameters set the location of the center of the ellipse, and the third and fourth parameters set the shape's width and height. If no height is specified, the value of width is used for both the width and height. If a negative height or width is specified, the absolute value is taken.",
+        code: "ellipse(x, y, w, [h]), ellipse(x, y, w, h, [detail])",
+        insert: "ellipse(${1:x}, ${2:y}, ${3:w}, ${4:h})",
+        syntax: [
+            {
+                parameter: "x: Number",
+                description: "x-coordinate of the center of ellipse"
+            },
+            {
+                parameter: "y: Number",
+                description: "y-coordinate of the center of ellipse"
+            }, 
+            {
+                parameter: "w: Number",
+                description: "width of the ellipse"
+            },
+            {
+                parameter: "h: Number",
+                description: "height of the ellipse"
+            },   
+            {
+                parameter: "detail: Integer",
+                description: "optional parameter for WebGL mode only. This is to specify the number of vertices that makes up the perimeter of the arc. Default value is 25. Won't draw a stroke for a detail of more than 50. (Optional)"
+            },            
+        ]
+    },
+    {
+        element: "circle",
+        description: "Draws a circle to the screen. A circle is a simple closed shape. It is the set of all points in a plane that are at a given distance from a given point, the centre. This function is a special case of the ellipse() function, where the width and height of the ellipse are the same.",
+        code: "circle(x, y, d)",
+        insert: "circle(${1:x}, ${2:y}, ${3:d})",
+        syntax: [
+            {
+                parameter: "x: Number",
+                description: "x-coordinate of the center of circle"
+            },
+            {
+                parameter: "y: Number",
+                description: "y-coordinate of the center of circle"
+            }, 
+            {
+                parameter: "d: Number",
+                description: "diameter of the circle"
+            },          
+        ]
+    },
+    {
+        element: "line",
+        description: "Draws a line (a direct path between two points) to the screen. If called with only 4 parameters, it will draw a line in 2D with a default width of 1 pixel. This width can be modified by using the strokeWeight() function. A line cannot be filled, therefore the fill() function will not affect the color of a line. So to color a line, use the stroke() function.",
+        code: "line(x1, y1, x2, y2), line(x1, y1, z1, x2, y2, z2)",
+        insert: "line(${1:x1}, ${2:y1}, ${3:x2}, ${4:y2})",
+        syntax: [
+            {
+                parameter: "x1: Number",
+                description: "the x-coordinate of the first point"
+            },
+            {
+                parameter: "y1: Number",
+                description: "the y-coordinate of the first point"
+            }, 
+            {
+                parameter: "x2: Number",
+                description: "the x-coordinate of the second point"
+            },
+            {
+                parameter: "y2: Number",
+                description: "the y-coordinate of the second point"
+            },             {
+                parameter: "z1: Number",
+                description: "the x-coordinate of the first point"
+            },
+            {
+                parameter: "z2: Number",
+                description: "the z-coordinate of the second point"
+            },        
+        ]
+    },
+    {
+        element: "circle",
+        description: "Draws a point, a coordinate in space at the dimension of one pixel. The first parameter is the horizontal value for the point, the second param is the vertical value for the point. The color of the point is changed with the stroke() function. The size of the point can be changed with the strokeWeight() function.",
+        code: "point(x, y, [z]), point(coordinate_vector)",
+        insert: "point(${1:x}, ${2:y})",
+        syntax: [
+            {
+                parameter: "x: Number",
+                description: "the x-coordinate"
+            },
+            {
+                parameter: "y: Number",
+                description: "the y-coordinate"
+            }, 
+            {
+                parameter: "z: Number",
+                description: "the z-coordinate (for WebGL mode) (Optional)"
+            },  
+            {
+                parameter: "coordinate_vector: p5.Vector",
+                description: "the coordinate vector"
+            },            
+        ]
+    },
+    {
+        element: "quad",
+        description: "Draws a quad on the canvas. A quad is a quadrilateral, a four sided polygon. It is similar to a rectangle, but the angles between its edges are not constrained to ninety degrees. The first pair of parameters (x1,y1) sets the first vertex and the subsequent pairs should proceed clockwise or counter-clockwise around the defined shape. z-arguments only work when quad() is used in WEBGL mode.",
+        code: "quad(x1, y1, x2, y2, x3, y3, x4, y4, [detailX], [detailY]), quad(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, [detailX], [detailY])",
+        insert: "quad(${1:x1}, ${2:y1}, ${3:x2}, ${4:y2}, ${5:x3}, ${6:y3}, ${7:x4}, ${8:y4})",
+        syntax: [
+            {
+                parameter: "x1: Number",
+                description: "the x-coordinate of the first point"
+            },
+            {
+                parameter: "y1: Number",
+                description: "the y-coordinate of the first point"
+            }, 
+            {
+                parameter: "x2: Number",
+                description: "the x-coordinate of the second point"
+            },
+            {
+                parameter: "y2: Number",
+                description: "the y-coordinate of the second point"
+            },
+            {
+                parameter: "x3: Number",
+                description: "the x-coordinate of the third point"
+            },
+            {
+                parameter: "y3: Number",
+                description: "the y-coordinate of the third point"
+            },     
+            {
+                parameter: "x4: Number",
+                description: "the x-coordinate of the fourth point"
+            },
+            {
+                parameter: "y4: Number",
+                description: "the y-coordinate of the fourth point"
+            },
+            {
+                parameter: "detailX: Integer",
+                description: "number of segments in the x-direction (Optional)"
+            },
+            {
+                parameter: "detailY: Integer",
+                description: "number of segments in the y-direction (Optional)"
+            }, 
+            {
+                parameter: "z1: Number",
+                description: "the z-coordinate of the first point"
+            },
+            {
+                parameter: "z2: Number",
+                description: "the z-coordinate of the second point"
+            },     
+            {
+                parameter: "z3: Number",
+                description: "the z-coordinate of the third point"
+            },
+            {
+                parameter: "z4: Number",
+                description: "the z-coordinate of the fourth point"
+            },  
+        ]
+    },
+    {
+        element: "rect",
+        description: "Draws a rectangle on the canvas. A rectangle is a four-sided closed shape with every angle at ninety degrees. By default, the first two parameters set the location of the upper-left corner, the third sets the width, and the fourth sets the height. The way these parameters are interpreted, may be changed with the rectMode() function.",
+        code: "rect(x, y, w, [h], [tl], [tr], [br], [bl]), rect(x, y, w, h, [detailX], [detailY])",
+        insert: "rect(${1:x}, ${2:y}, ${3:w})",
+        syntax: [
+            {
+                parameter: "x: Number",
+                description: "x-coordinate of the rectangle"
+            },
+            {
+                parameter: "y: Number",
+                description: "y-coordinate of the rectangle"
+            }, 
+            {
+                parameter: "w: Number",
+                description: "width of the rectangle"
+            },  
+            {
+                parameter: "tl: Number",
+                description: "height of the rectangle. (Optional)"
+            },       
+            {
+                parameter: "tr: Number",
+                description: "optional radius of top-right corner. (Optional)"
+            },       
+            {
+                parameter: "br: Number",
+                description: "optional radius of bottom-right corner. (Optional)"
+            },       
+            {
+                parameter: "bl: Number",
+                description: "optional radius of bottom-left corner. (Optional)"
+            },       
+            {
+                parameter: "detailX: Integer",
+                description: "number of segments in the x-direction (Optional)"
+            },
+            {
+                parameter: "detailY: Integer",
+                description: "number of segments in the y-direction (Optional)"
+            },   
+        ]
+    },
+    {
+        element: "square",
+        description: "Draws a square to the screen. A square is a four-sided shape with every angle at ninety degrees, and equal side size. This function is a special case of the rect() function, where the width and height are the same, and the parameter is called s for side size. By default, the first two parameters set the location of the upper-left corner, the third sets the side size of the square.",
+        code: "square(x, y, s, [tl], [tr], [br], [bl])",
+        insert: "square(${1:x}, ${2:y}, ${3:s})",
+        syntax: [
+            {
+                parameter: "x: Number",
+                description: "x-coordinate of the square"
+            },
+            {
+                parameter: "y: Number",
+                description: "y-coordinate of the square"
+            }, 
+            {
+                parameter: "s: Number",
+                description: "side size of the square"
+            },  
+            {
+                parameter: "tl: Number",
+                description: "height of the rectangle. (Optional)"
+            },       
+            {
+                parameter: "tr: Number",
+                description: "optional radius of top-right corner. (Optional)"
+            },       
+            {
+                parameter: "br: Number",
+                description: "optional radius of bottom-right corner. (Optional)"
+            },       
+            {
+                parameter: "bl: Number",
+                description: "optional radius of bottom-left corner. (Optional)"
+            },         
+        ]
+    },
+    {
+        element: "triangle",
+        description: "Draws a triangle to the canvas. A triangle is a plane created by connecting three points. The first two arguments specify the first point, the middle two arguments specify the second point, and the last two arguments specify the third point.",
+        code: "triangle(x1, y1, x2, y2, x3, y3)",
+        insert: "triangle(${1:x1}, ${2:y1}, ${3:x2}, ${4:y2}, ${5:x3}, ${6:y3}",
+        syntax: [
+            {
+                parameter: "x1: Number",
+                description: "the x-coordinate of the first point"
+            },
+            {
+                parameter: "y1: Number",
+                description: "the y-coordinate of the first point"
+            }, 
+            {
+                parameter: "x2: Number",
+                description: "the x-coordinate of the second point"
+            },
+            {
+                parameter: "y2: Number",
+                description: "the y-coordinate of the second point"
+            },
+            {
+                parameter: "x3: Number",
+                description: "the x-coordinate of the third point"
+            },
+            {
+                parameter: "y3: Number",
+                description: "the y-coordinate of the third point"
+            },     
+        ]
+    },
 ];
